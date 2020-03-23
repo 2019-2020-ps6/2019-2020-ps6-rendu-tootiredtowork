082bb401947acf1c95ec52fce94fe3ba16d7f291
ThomasMartin710 <thomas.martin710@outlook.fr>
2020-03-23 20:32:03 +0100

    changement de page + suppression truc inutile

--- src/app/configuration/configuration.component.ts
+++ src/app/configuration/configuration.component.ts
@@ -1,35 +1,23 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-    selector: 'app-configuration',
-    templateUrl: './configuration.component.html',
-    styleUrls: ['./configuration.component.scss']
+  selector: 'app-configuration',
+  templateUrl: './configuration.component.html',
+  styleUrls: ['./configuration.component.scss']
 })
 export class ConfigurationComponent implements OnInit {
 
-    fontSize: number = 10;
-    margeSize: number = 10;
-    appareilStatus: string='allumé';
+  fontSize: number = 10;
 
-    constructor() {
-
-    setTimeout(
-      () => {
-        this.appareilStatus = 'éteint';
-      }, 4000
-    );
+  constructor() {
   }
-    
-    ngOnInit() {
-    }
 
-    goQuiz() {
-        console.log("ok");
-    }
+  ngOnInit() {
+  }
 
-    changeSize(){
-        return this.fontSize+'px';
-    }
+  changeSize() {
+    return this.fontSize + 'px';
+  }
 
 
 }
