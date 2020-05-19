c06f829061287ec59fcf18ed23669e82b00e688e
Vivian Losciale <vivian.losciale@etu.univ-cotedazur.fr>
2020-05-19 20:04:26 +0200

    fix taille police + rapport

--- src/app/dialogs/delete/delete-dialog.component.ts
+++ src/app/dialogs/delete/delete-dialog.component.ts
@@ -1,6 +1,7 @@
 import { MatDialogRef } from '@angular/material/dialog';
 import { Component,Inject } from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { ConfigurationService } from 'src/services/configuration.service';
 
 @Component({
     selector: 'app-delete-dialog',
@@ -8,7 +9,7 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
     styleUrls: ['./delete-dialog.component.scss']
 })
 export class DeleteDialog {
-    constructor(@Inject(MAT_DIALOG_DATA) public data: any,public dialogRef: MatDialogRef<DeleteDialog>) { }
+    constructor(@Inject(MAT_DIALOG_DATA) public data: any,public dialogRef: MatDialogRef<DeleteDialog>, public configService: ConfigurationService) { }
 
     closeDialog() {
         this.dialogRef.close('Pizza!');
--- src/app/dialogs/fill/fill-dialog.component.ts
+++ src/app/dialogs/fill/fill-dialog.component.ts
@@ -1,5 +1,6 @@
 import { MatDialogRef } from '@angular/material/dialog';
 import { Component } from '@angular/core';
+import { ConfigurationService } from 'src/services/configuration.service';
 
 @Component({
     selector: 'app-fill-dialog',
@@ -9,7 +10,7 @@ import { Component } from '@angular/core';
 
 //pour remplir tout les champs pour creer un element
 export class FillDialog {
-    constructor(public dialogRef: MatDialogRef<FillDialog>) { }
+    constructor(public dialogRef: MatDialogRef<FillDialog>, public configService: ConfigurationService) { }
 
     closeDialog() {
     	this.dialogRef.close();
